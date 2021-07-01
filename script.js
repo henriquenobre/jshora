b84ca2dcdd1d7d1f1db814e0684219063c1a07f4
henriquenobre <83713793+henriquenobre@users.noreply.github.com>
2021-07-01 20:58:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ function carregar(){
     var img = document.getElementById('imagem')
     var hora = document.getElementById('qhora')
     var hora = Number(qhora.value)
-    msg.innerHTML = `${hora} horas`
+    
     
     if(hora >= 0 && hora < 12){
+        msg.innerHTML = `${hora} horas`
         img.src = 'dia.jpg'
         document.body.style.background = '#e2cd9f'
         oi.innerHTML = 'Bom dia'
     }
     else if(hora >= 12 && hora <= 18){
+        msg.innerHTML = `${hora} horas`
         img.src = 'tarde.jpg'
         document.body.style.background = '#b9846f'
         oi.innerHTML = 'Boa tarde'
     }
     else if(hora > 18 && hora <= 24){
+        msg.innerHTML = `${hora} horas`
         img.src = 'noite.jpg'
         document.body.style.background = '#515154'
         oi.innerHTML = 'Boa noite'
